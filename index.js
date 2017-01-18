eb0f7cfcf768036a2e6b11c239b521a794a85e97
Matt Fender <mjfender@gmail.com>
2017-01-18 16:41:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+    return counter
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    getBumps,
+    addBump
+  }
+}
+/* const { addBump, getBumps } = bumpCounter() 
+this works because it deconstructs the bumpCounter() into its two separate 'shortcuts' that are connected. Note that it doesn't work if you do the const declarations separately.
+*/
+
+function createAnimal(animalType) {
+  
+  return function createMonster(deadlyDevice) {
+    return {
+    animalType,
+    deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
+
